fb8eee4c70d8eb9ca5b0e9f4a6644886529725ef
Puleya <320901919@qq.com>
2020-11-30 16:23:32 +0800

    初始化

---
+++ project/privilegeService/src/main/java/demo/Controller/PrivilegeController.java
@@ -0,0 +1,12 @@
+package demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chei1
+ */
+@RestController
+@RequestMapping(value = "/privilege", produces = "application/json;charset=UTF-8")
+public class PrivilegeController {
+}
