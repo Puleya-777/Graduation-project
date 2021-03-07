547d6c2fe982e340a9b586fcc1ba078ee723a19c
林尚政 <2223012067@qq.com>
2021-03-07 20:58:26 +0800

    goods pageInfo的list切片  by lsz

--- project/goods/src/main/java/com/example/util/CommonUtil.java
+++ project/goods/src/main/java/com/example/util/CommonUtil.java
@@ -4,13 +4,24 @@ import com.example.model.bo.CouponActivity;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CommonUtil<T> {
 
     public PageInfo<T> listToPage(List<T> list,Integer page,Integer pageSize){
-        list=list.subList((page-1)*pageSize,page*pageSize-1);
+        int left=(page-1)*pageSize;
+        int right=page*pageSize;
+        if(list.size()>=right){
+            list=list.subList(left,right);
+        }else{
+            if(left>=list.size()){
+                list=new ArrayList<>();
+            }else{
+                list=list.subList(left,list.size());
+            }
+        }
         PageInfo<T> retPage=new PageInfo<>(list);
         retPage.setPages(page);
         retPage.setPageNum(page);
